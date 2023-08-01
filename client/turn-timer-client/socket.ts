062334110ab0c33ec5c8c50595bb3b8891a00b3e
David <david@sirrus7.com>
2023-08-01 15:08:05 -0400

    ec2

--- client/turn-timer-client/socket.ts
+++ client/turn-timer-client/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
-const URL: string = import.meta.env.NODE_ENV === 'production' ? '' : 'http://127.0.0.1/3000'; //'http://localhost:3000';
+const URL: string = import.meta.env.NODE_ENV === 'production' ? '' : 'http://3.83.94.212/3000'; //'http://localhost:3000';
 
 export const socket = io(URL, {autoConnect: false});
