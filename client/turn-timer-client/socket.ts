6ad3c3d89adf3bae73ef89971322b7c71c8ee2ec
David <david@sirrus7.com>
2023-08-08 15:16:56 -0400

    elastic ip

--- client/turn-timer-client/socket.ts
+++ client/turn-timer-client/socket.ts
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL: string = import.meta.env.NODE_ENV === 'production' ? '' : 'http://3.83.94.212/3000'; //'http:
 // const URL: string = 'http://192.168.0.25:3000';
-const URL: string = 'https://3.83.94.212';
+const URL: string = 'https://54.175.61.41';
 
 export const socket = io(URL, {autoConnect: false});
